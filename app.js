4851b7e10f64dc199b9549fca99978eed1257adf
Yutaka Ogasawara <y.ogagaga@gmail.com>
2014-01-02 13:35:07 +0900

    Fixed doSearch function
    add YouTube Search API

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var mainCtrl = function($scope, $http) {
+  $scope.doSearch = function() {
+    var url = 'http://gdata.youtube.com/feeds/api/videos?'
+        + [
+          'q=' + encodeURIComponent($scope.query),
+          'max-results=10',
+          'v=2',
+          'callback=JSON_CALLBACK'
+        ].join('&');
+    $http.jsonp(url).success(function(data){
+      console.dir(date);
+    });
+  }
 }
